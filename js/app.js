cd7208e708517da6672fc44ddc4d149f378bb311
Anastasia Puteeva <anastasiaputeeya@gmail.com>
2021-02-01 01:15:25 +0300

    Added a chess carousel

--- js/app.js
+++ js/app.js
@@ -13,4 +13,45 @@ $(function() {
     }).bind('close:finish', function(){
         $('.hamburger').removeClass('is-active');
     }); 
+    
+    $('.carousel-services').on('initialized.owl.carousel', function(){
+        setTimeout(function(){
+            carouselService()
+        }, 900);
+    });
+    
+    $('.carousel-services').owlCarousel({
+        
+        dots: false,
+        nav: true,
+        smartSpeed: 700,
+        navText:['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items: 1
+            },
+            800:{
+                items: 2
+            },
+            1100:{
+                items: 3
+            }
+        }    
+    });
+    
+    function carouselService(){
+        $('.carousel-services-item').each(function(){
+            var find_height = $(this).find('.carousel-services-content').outerHeight(), carousel_services_image=$(this).find('.carousel-services-image').css('min-height', find_height);
+        });
+    }
+    carouselService();
+    
+    //Resize Window
+    function onResize(){
+        $('.carousel-services-list').equalHeights();
+    } onResize();
+    
+    window.onresize = function(){onResize()};
+    
 });
